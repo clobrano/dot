ba7a3b81be8fe4eb9b2e0efdeb7336c701857727
Carlo Lobrano <c.lobrano@gmail.com>
2019-07-04 16:03:22 +0200

    git: add diff3 diff style configuration

--- .config/cconf/git/setup.sh
+++ .config/cconf/git/setup.sh
@@ -4,3 +4,4 @@
 git config --global user.name "Carlo Lobrano"
 git config --global user.email c.lobrano@gmail.com
 git config --global core.excludefiles ~/.gitignore_global
+git config --global merge.conflictStyle diff3 # include common ancestor

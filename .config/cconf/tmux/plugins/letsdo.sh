c3f6d9bc8414e97367b16178efa38a5aa97a5fef
Carlo Lobrano <c.lobrano@gmail.com>
2019-07-18 14:15:16 +0200

    tmux: add letsdo integration
    Add tmux/plugin folder under .config/cconf directory and letsdo.sh
    script that prints into the status line both task title and work time.

---
+++ .config/cconf/tmux/plugins/letsdo.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# -*- coding: UTF-8 -*-
+## Read current Letsdo task title from tmux
+
+# No Letsdo configuration
+[ ! -f $HOME/.letsdo ] && exit 0
+
+DATA_DIRECTORY=$(sed -n 's/DATA_DIRECTORY:\s*\(.*\)/\1/p' "$HOME/.letsdo")
+
+# no running tasks
+[ ! -f  "$DATA_DIRECTORY/letsdo-task" ] && exit 0
+
+task=$(sed -n 's_"name": "\(.*\)",_\1_p' "$DATA_DIRECTORY/letsdo-task")
+begin=$(date +%s -d "$(sed -n 's_"start": "\(.*\)"_\1_p' "$DATA_DIRECTORY/letsdo-task")")
+end=$(date +%s)
+# Why I need an 1h offset to get the right value? Is it for the daylight setting?
+echo $task [$(date +"%Hh %Mm" --date="@$(($end - $begin - 3600))")]

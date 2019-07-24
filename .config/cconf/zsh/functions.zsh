7566572dda5dbe55c8a160918b087edf09ca522e
Carlo Lobrano <c.lobrano@gmail.com>
2019-07-24 12:46:22 +0200

    zsh: improve long running process tracking function
    - unset CMD_START_TIME env variable at each new start
    - add the elapsed time in seconds in popup message

--- .config/cconf/zsh/functions.zsh
+++ .config/cconf/zsh/functions.zsh
@@ -1,7 +1,10 @@
 function preexec() {
     # Save the name of the command and the time when it starts
     CMD_NAME=$1
+    unset CMD_START_TIME
 
+    # it seems that short name like "n" do not work in zsh, while this
+    # function works fine tested in bash, try to avoid them, e.g. use nvim
     WHITELIST="tig fzf fn f nvim ndm nd n vim vng vg v gvim"
     if [[ $WHITELIST =~ (^|[[:space:]])$CMD_NAME($|[[:space:]]) ]]; then
         return
@@ -19,7 +22,7 @@ function precmd() {
     CMD_ELAPSED_TIME=$(($CMD_STOP_TIME - $CMD_START_TIME))
     THRESHOLD=60
     if [[ $CMD_ELAPSED_TIME -gt $THRESHOLD ]]; then
-        notify-send -i time 'job finished' "$CMD_NAME"
+        notify-send -i time 'job finished' "$CMD_NAME: $CMD_ELAPSED_TIME seconds"
     fi
     unset CMD_START_TIME
     unset CMD_STOP_TIME

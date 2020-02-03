221183d0b6128c0c0a4896fae56bfb7bdafb51ff
Carlo Lobrano <c.lobrano@gmail.com>
2020-02-03 09:52:56 +0100

    fonts: update installer with Monaco fonts

--- .config/cconf/fonts/install-fonts.sh
+++ .config/cconf/fonts/install-fonts.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 # -*- coding: UTF-8 -*-
+set -e
 
-git clone https://github.com/powerline/fonts.git --depth=1 $HOME/.powerline-fonts
-./$HOME/.powerline-fonts/install.sh
+#git clone https://github.com/powerline/fonts.git --depth=1 $HOME/.powerline-fonts
+#./$HOME/.powerline-fonts/install.sh
+#rm -r $HOME/.powerline-fonts
+
+wget $HOME/.local/share/fonts https://github.com/hbin/top-programming-fonts/raw/master/Monaco-Linux.ttf
+
 fc-cache -vf

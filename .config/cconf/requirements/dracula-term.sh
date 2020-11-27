688b66ee570635ccfabead8cf5817057b19738f0
Carlo Lobrano <c.lobrano@gmail.com>
2020-11-27 18:22:37 +0100

    add script for dracula gnome-terminal colorscheme

---
+++ .config/cconf/requirements/dracula-term.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# -*- coding: UTF-8 -*-
+sudo apt-get install dconf-cli
+git clone https://github.com/dracula/gnome-terminal ~/workspace/toolkit/dracula-gnome-terminal
+pushd ~/workspace/toolkit/dracula-gnome-terminal
+./install.sh
+popd

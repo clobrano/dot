415bec88201a0e6e883600c52fd4b8be5955215f
Carlo Lobrano <c.lobrano@gmail.com>
2021-04-20 10:21:59 +0200

    Add exercism setup script

---
+++ .config/cconf/requirements/exercism.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# -*- coding: UTF-8 -*-
+
+set -ex
+
+version=3.0.13
+package=exercism-$version-linux-x86_64.tar.gz
+
+echo [+] getting Exercism $version CLI
+wget https://github.com/exercism/cli/releases/download/v$version/$package
+tar -xf $package
+mkdir -p ~/.local/share/bin
+chmod +x exercism
+mv exercism ~/.local/share/bin
+rm $package

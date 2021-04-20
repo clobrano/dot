f358f87f47ca86767c8febba58f8fe928577d9d2
Carlo Lobrano <c.lobrano@gmail.com>
2021-04-20 10:02:16 +0200

    batch-install: add distribution check

--- .config/cconf/requirements/batch-install.sh
+++ .config/cconf/requirements/batch-install.sh
@@ -2,14 +2,20 @@
 # Install all the package in the provided input file.
 # The input file shall contain one package per line.
 
+set -eu
+distro=`cat /etc/os-release | grep -E "^NAME" | cut -d'=' -f2`
+
 [ ! -f "$1" ] && echo "$1 is not a valid file" && exit 1
 
-set -eu
 FILEPATH=$1
 
-for d in $(awk '/^\s*[^#]/' "$FILEPATH"); do
-    if [ 0 == $(dpkg -l | grep $d | wc -l) ]; then
-        echo installing $d...
-        sudo apt-get install -y $d
-    fi
-done
+if [[ "$distro" == "Ubuntu" ]]; then
+    for d in $(awk '/^\s*[^#]/' "$FILEPATH"); do
+        if [ 0 == $(dpkg -l | grep $d | wc -l) ]; then
+            echo installing $d...
+            sudo apt-get install -y $d
+        fi
+    done
+else
+    echo [!] $distro distribution is not supported yet.
+fi

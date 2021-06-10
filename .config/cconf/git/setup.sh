381451621ffddf6e3f18832d95274290ba973dd0
Carlo Lobrano <c.lobrano@gmail.com>
2021-06-10 08:50:33 +0200

    Add git aliases

--- .config/cconf/git/setup.sh
+++ .config/cconf/git/setup.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 # -*- coding: UTF-8 -*-
 
+set -x
 git config --global user.name "Carlo Lobrano"
 git config --global user.email c.lobrano@gmail.com
 git config --global core.excludesfile ~/.gitignore_global
-git config --global core.editor gedit
+git config --global core.editor nvim
 git config --global merge.conflictStyle diff3 # include common ancestor
+
+git config --global alias.co checkout
+git config --global alias.cm commit
+git config --global alias.sw switch
+git config --global alias.st status
+git config --global alias.sh stash
+

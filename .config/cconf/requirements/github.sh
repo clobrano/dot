2c6ecdc3d88512ae6027eb874ce19432cf880bef
Carlo Lobrano <c.lobrano@gmail.com>
2021-09-25 22:57:20 +0200

    github: add dnf support to installer script

--- .config/cconf/requirements/github.sh
+++ .config/cconf/requirements/github.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 # -*- coding: UTF-8 -*-
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
-sudo apt-add-repository https://cli.github.com/packages
-sudo apt update
-sudo apt install gh
+
+if [[ $(which apt | wc -l) = "1" ]]; then
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+    sudo apt-add-repository https://cli.github.com/packages
+    sudo apt update
+    sudo apt install gh
+fi
+
+if [[ $(which dnf | wc -l) = "1" ]]; then
+    sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+    sudo dnf install gh
+fi

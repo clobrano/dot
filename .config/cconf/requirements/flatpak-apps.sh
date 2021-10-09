48cbe81a96f86ea5c8b076cabad150b05bd2808c
Carlo Lobrano <c.lobrano@gmail.com>
2021-10-09 19:38:44 +0200

    req: add script to install common flatpak apps

---
+++ .config/cconf/requirements/flatpak-apps.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# -*- coding: UTF-8 -*-
+set -x
+flatpak install flathub com.nextcloud.desktopclient.nextcloud
+flatpak install flathub md.obsidian.Obsidian
+flatpak install flathub me.kozec.syncthingtk

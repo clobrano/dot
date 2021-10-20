9dd4e305aeaed0188fd68397c3cdb86d363c6903
Carlo Lobrano <c.lobrano@gmail.com>
2021-10-20 09:27:08 +0200

    nvim: lsp: update python script

--- .config/cconf/requirements/lsp/python-lsp.sh
+++ .config/cconf/requirements/lsp/python-lsp.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 # -*- coding: UTF-8 -*-
 set -x
-pip3 install --user python-lsp-server
-pip3 install --user python-lsp-black
-pip3 install --user autopep8
-pip3 install --user rope
+pip3 install --user 'python-lsp-server[all]'

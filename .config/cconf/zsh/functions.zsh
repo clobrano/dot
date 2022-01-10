dfad179b757dfe36e7c0a600db73970b6ecd072f
Carlo Lobrano <carlo.lobrano@telit.com>
2022-01-10 10:15:29 +0100

    zsh: add "fd" function to preview git repo with fzf

--- .config/cconf/zsh/functions.zsh
+++ .config/cconf/zsh/functions.zsh
@@ -1,3 +1,8 @@
+function fd() {
+  preview="git diff $@ --color=always -- {-1}"
+  git diff $@ --name-only | fzf -m --ansi --preview $preview
+}
+
 function mkdir() {
     # extend mkdir with custom features
     # propose adding "--parents" flag.

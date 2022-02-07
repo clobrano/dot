bd24a0ba8f4151a26aed24c1a8850cc5d0ad897e
Carlo Lobrano <c.lobrano@gmail.com>
2022-02-07 12:15:53 +0100

    environments: rust: update installer with checks and run tests

--- .config/cconf/environments/rust-install.sh
+++ .config/cconf/environments/rust-install.sh
@@ -1,4 +1,25 @@
 #!/usr/bin/env bash
 # Rust installer
+version=`rustc --version`
+if [[ -n $version ]]; then
+    echo [+] Rust installed
+    echo $version
+else
+    set -x
+    curl https://sh.rustup.rs -sSf | sh
+    source $HOME/.cargo/env
+fi
 
-curl https://sh.rustup.rs -sSf | sh
+# Rust test code
+cd /tmp
+RUSTFILE=rusttestfile
+cat > $RUSTFILE.rs <<EOF
+fn main() {
+    println!("Rust is working fine!");
+}
+EOF
+
+rustc $RUSTFILE.rs
+./$RUSTFILE
+rm ./$RUSTFILE{,.rs}
+cd -

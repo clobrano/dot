cc996c177fede2dcd60f1ee19f9f097a8b35cda1
Carlo Lobrano <c.lobrano@gmail.com>
2022-02-22 09:08:49 +0100

    environments: rust: rust-analyzer installer

--- .config/cconf/environments/rust-install.sh
+++ .config/cconf/environments/rust-install.sh
@@ -23,3 +23,8 @@ rustc $RUSTFILE.rs
 ./$RUSTFILE
 rm ./$RUSTFILE{,.rs}
 cd -
+
+echo "Installing rust-analyzer"
+mkdir -p ~/.local/bin
+curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.local/bin/rust-analyzer
+chmod +x ~/.local/bin/rust-analyzer

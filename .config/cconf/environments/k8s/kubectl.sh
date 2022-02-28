fae0cdc06a5f7b7257da17b7fa863acf843df300
Carlo Lobrano <c.lobrano@gmail.com>
2022-02-28 12:06:33 +0100

    environments: add k8s environments scripts
    add installer for kind and kubectl

---
+++ .config/cconf/environments/k8s/kubectl.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+# -*- coding: UTF-8 -*-
+## Helper script to install/manage kubectl
+## options:
+##     -i, --install   Install kind binary from release
+
+# CLInt GENERATED_CODE: start
+
+# No-arguments is not allowed
+[ $# -eq 0 ] && sed -ne 's/^## \(.*\)/\1/p' $0 && exit 1
+
+# Converting long-options into short ones
+for arg in "$@"; do
+  shift
+  case "$arg" in
+"--install") set -- "$@" "-i";;
+  *) set -- "$@" "$arg"
+  esac
+done
+
+function print_illegal() {
+    echo Unexpected flag in command line \"$@\"
+}
+
+# Parsing flags and arguments
+while getopts 'hi' OPT; do
+    case $OPT in
+        h) sed -ne 's/^## \(.*\)/\1/p' $0
+           exit 1 ;;
+        i) _install=1 ;;
+        \?) print_illegal $@ >&2;
+            echo "---"
+            sed -ne 's/^## \(.*\)/\1/p' $0
+            exit 1
+            ;;
+    esac
+done
+# CLInt GENERATED_CODE: end
+
+if [[ -n $_install ]]; then
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    chmod +x ./kubectl
+    mv ./kubectl $HOME/.local/bin/kubectl
+fi
+
+if [[ -f $HOME/.local/bin/kubectl ]]; then
+    $HOME/.local/bin/kubectl version --client
+fi

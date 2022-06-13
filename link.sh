1dde649a26df73f03be8f8630cd1ebb9c7c6c2e8
Carlo Lobrano <c.lobrano@gmail.com>
2022-06-13 15:07:55 +0200

    dot: link script to only link files, ask for permission and link new lua folder

--- link.sh
+++ link.sh
@@ -3,60 +3,6 @@
 ## helper script to track dotfiles for later backup
 ## dot will move the original dotfiles in a given directory for backup
 ## and replace it with a symlink.
-## options:
-##    -d, --deploy          Deploy dotfiles in HOME directory
-##    -i, --install         Install programs and dependencies
-
-
-# CLInt GENERATED_CODE: start
-# info: https://github.com/clobrano/CLInt.git
-
-# No-arguments is not allowed
-[ $# -eq 0 ] && sed -ne 's/^## \(.*\)/\1/p' $0 && exit 1
-
-# Converting long-options into short ones
-for arg in "$@"; do
-  shift
-  case "$arg" in
-"--deploy") set -- "$@" "-d";;
-"--install") set -- "$@" "-i";;
-  *) set -- "$@" "$arg"
-  esac
-done
-
-function print_illegal() {
-    echo Unexpected flag in command line \"$@\"
-}
-
-# Parsing flags and arguments
-while getopts 'hdi' OPT; do
-    case $OPT in
-        h) sed -ne 's/^## \(.*\)/\1/p' $0
-           exit 1 ;;
-        d) _deploy=1 ;;
-        i) _install=1 ;;
-        \?) print_illegal $@ >&2;
-            echo "---"
-            sed -ne 's/^## \(.*\)/\1/p' $0
-            exit 1
-            ;;
-    esac
-done
-# CLInt GENERATED_CODE: end
-
-install() {
-    pushd ${HOME}/.dot/.config/cconf/environments
-
-    # install basic programs
-    ./batch-install.sh req-base.txt
-    ./github-cli-install.sh
-    ./lsp/python-lsp.sh
-    ./lsp/c-cpp.lsp.sh
-    ./z-directory-jump.sh
-    ./neovim-from-source.sh
-    ./vim-plug-install.sh
-    popd
-}
 
 deploy() {
     to_link=(
@@ -75,20 +21,37 @@ deploy() {
         ".config/nvim/ginit.vim"
         ".config/nvim/init.vim"
         ".config/nvim/plugin"
+        ".config/nvim/lua"
         ".config/nvim/spell"
         ".config/nvim/thesaurus"
-        ".config/nvim/UltiSnips"
     )
 
     # nvim directory needs to be created first
     mkdir -pv ${HOME}/.config/nvim
 
     SRC=$(pwd)
+    
     for file in ${to_link[@]}; do
-        echo ln -s ${SRC}/${file} ${HOME}/${file}
-        ln -s ${SRC}/${file} ${HOME}/${file}
-    done
+        if [[ -e ${HOME}/${file} ]]; then
+            confirmed=false
+            while ! $confirmed; do
+                echo [+] ${HOME}/${file} exists, override [y/n]?
+                read confirm
+                case $confirm in
+                    n|y)
+                        confirmed=true
+                        [[ $confirm == "n" ]] && continue
+                        echo ln -fs ${SRC}/${file} ${HOME}/${file}
+                        ln -fs ${SRC}/${file} ${HOME}/${file}
+                        ;;
+                    *)
+                        echo [!] please write 'y' or 'n'
+                        ;;
+                esac
+            done
+        fi
+            done
 }
 
-[ ! -z $_install ] && install
-[ ! -z $_deploy ] && deploy
+# -- main --
+deploy

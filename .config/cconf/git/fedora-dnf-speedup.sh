9e3600108d4f5396ef49f35382054b5652d4f6b6
Carlo Lobrano <c.lobrano@gmail.com>
2022-06-16 10:11:57 +0200

    environments: script to speed dnf up

---
+++ .config/cconf/git/fedora-dnf-speedup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# -*- coding: UTF-8 -*-
+DNF_CONF=/etc/dnf/dnf.conf
+
+if grep max_parallel_downloads $DNF_CONF >/dev/null; then
+    echo [+] $DNF_CONF is already configured.
+    exit 0
+fi
+
+exit 1
+cat <<EOF >> 
+max_parallel_downloads=10
+fastestmirror=true
+EOF

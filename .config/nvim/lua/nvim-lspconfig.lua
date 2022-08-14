f77fc88aefe6e3bb0431b38f62c80d47a97d2db4
Carlo Lobrano <c.lobrano@gmail.com>
2022-08-14 15:00:22 +0200

    nvim: lsp: cleanup lsp config

--- .config/nvim/lua/nvim-lspconfig.lua
+++ .config/nvim/lua/nvim-lspconfig.lua
@@ -1,23 +1,15 @@
-require'lspconfig'.clangd.setup{}
-
-local on_attach = function(client, bufnr)
-    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-    -- Enable completion triggered by <c-x><c-o>
-    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-end
--- Setup lspconfig.
---require('lspconfig').clangd.setup {
---capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
---}
-
-
---require('lspconfig').pylsp.setup{}
-require('lspconfig').gopls.setup{}
-require('lspconfig').rust_analyzer.setup({})
-
-
 lspconfig = require'lspconfig'
+
+lspconfig.clangd.setup{
+    default_config = {
+        cmd = { 'clangd -header-insertion=never' }
+    } 
+}
+
+lspconfig.gopls.setup{}
+
+lspconfig.rust_analyzer.setup({})
+
 lspconfig.pylsp.setup{
   settings = {
     formatCommand = {"black"},

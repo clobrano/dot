5fe91384c5d238a6e6b9a2b7d2eff8cdcab8f41f
Carlo Lobrano <c.lobrano@gmail.com>
2022-08-22 12:04:08 +0200

    tmux: letsdo: shorter job description

--- .config/cconf/tmux/plugins/letsdo.sh
+++ .config/cconf/tmux/plugins/letsdo.sh
@@ -12,8 +12,8 @@ DATA_DIRECTORY=$(sed -n 's/DATA_DIRECTORY:\s*\(.*\)/\1/p' "$HOME/.letsdo")
 
 task=$(sed -n 's_"name": "\(.*\)",_\1_p' "$DATA_DIRECTORY/letsdo-task")
 task_len=${#task}
-if [[ $task_len > 50 ]]; then
-    task=${task:0:47}...
+if [[ $task_len > 30 ]]; then
+    task=${task:0:27}...
 else
     # preserve the space after task name.
     task="${task} "

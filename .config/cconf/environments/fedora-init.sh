495a53084feccca108d1a39cceee82a48b354546
Carlo Lobrano <c.lobrano@gmail.com>
2022-11-11 10:11:05 +0100

    environments: fedora init script add media codec step

--- .config/cconf/environments/fedora-init.sh
+++ .config/cconf/environments/fedora-init.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 # Initial setup for Fedora
-ENVIRONMENT=~/.dot/.config/cconf/environments
+ENVIRONMENT=$HOME/.dot/.config/cconf/environments
 
+pushd $HOME/.dot
 set -x
 git submodule update --init
 $ENVIRONMENT/dnf-speed-up.sh
 $ENVIRONMENT/dnf-install-base-requirements.sh
 $ENVIRONMENT/fedora-enable-rpm-fusion.sh
+$ENVIRONMENT/fedora-install-media-codecs.sh
 $ENVIRONMENT/flatpak-install-apps.sh
 
-./link.sh
+$HOME/.dot/link.sh
 $ENVIRONMENT/vim-plug-install.sh
+popd
 nvim

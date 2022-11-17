2d5aad10fe7ad8c0a1aa77bba605a48db882e5f8
Carlo Lobrano <c.lobrano@gmail.com>
2022-11-17 10:08:08 +0100

    go: bump version for installer script to 1.19.3

--- .config/cconf/environments/golang.sh
+++ .config/cconf/environments/golang.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 # Install golang runtime
-VERSION=1.18.3
+VERSION=1.19.3
 URL=https://go.dev/dl/go${VERSION}.linux-amd64.tar.gz
+
+set -e
+
+# Get the tarball
 wget --tries=3 --no-clobber $URL
+
+# Remove already installed go
 sudo rm -rf /usr/local/go
+
+# Copy new version
 sudo tar -C /usr/local -xzf go${VERSION}.linux-amd64.tar.gz
-[[ $? == 0 ]] && rm go${VERSION}.linux-amd64.tar.gz
 /usr/local/go/bin/go version
+
+# Clean the environment
+[[ $? == 0 ]] && rm go${VERSION}.linux-amd64.tar.gz

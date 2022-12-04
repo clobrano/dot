861f596d27064627f998db2d8a93167a786e3f9b
Carlo Lobrano <c.lobrano@gmail.com>
2022-12-04 19:01:15 +0100

    environment: kubectl requires local/bin dir

--- .config/cconf/environments/kubernetes/kubectl.sh
+++ .config/cconf/environments/kubernetes/kubectl.sh
@@ -40,6 +40,7 @@ done
 if [[ -n $_install ]]; then
     curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
     chmod +x ./kubectl
+    mkdir -p $HOME/.local/bin/
     mv ./kubectl $HOME/.local/bin/kubectl
 fi
 

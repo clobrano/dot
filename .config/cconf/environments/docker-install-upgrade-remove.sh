c2d4ba8adf7042c03888095190e78a456e461d49
Carlo Lobrano <c.lobrano@gmail.com>
2022-12-04 19:02:19 +0100

    environment: improve docker installer

---
+++ .config/cconf/environments/docker-install-upgrade-remove.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+# -*- coding: UTF-8 -*-
+## Convenience script to install, update and remove Docker
+## options
+##     -i, --install
+##     -r, --remove
+
+# CLInt GENERATED_CODE: start
+# info: https://github.com/clobrano/CLInt.git
+
+# No-arguments is not allowed
+[ $# -eq 0 ] && sed -ne 's/^## \(.*\)/\1/p' $0 && exit 1
+
+# Converting long-options into short ones
+for arg in "$@"; do
+  shift
+  case "$arg" in
+"--install") set -- "$@" "-i";;
+"--remove") set -- "$@" "-r";;
+  *) set -- "$@" "$arg"
+  esac
+done
+
+function print_illegal() {
+    echo Unexpected flag in command line \"$@\"
+}
+
+# Parsing flags and arguments
+while getopts 'hir' OPT; do
+    case $OPT in
+        h) sed -ne 's/^## \(.*\)/\1/p' $0
+           exit 1 ;;
+        i) _install=1 ;;
+        r) _remove=1 ;;
+        \?) print_illegal $@ >&2;
+            echo "---"
+            sed -ne 's/^## \(.*\)/\1/p' $0
+            exit 1
+            ;;
+    esac
+done
+# CLInt GENERATED_CODE: end
+
+install () {
+    # install using the "convenience script"
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    chmod u+x ./get-docker.sh
+    ./get-docker.sh
+    sudo systemctl enable docker
+    sudo usermod -G docker -a $USER
+    newgrp docker
+    sudo systemctl restart docker
+    docker version
+    docker run --rm hello-world
+}
+
+remove () {
+    echo remove
+    return
+    sudo dnf remove docker-ce docker-ce-cli containerd.io docker-compose-plugin
+    sudo rm -rf /var/lib/docker
+    sudo rm -rf /var/lib/containerd
+}
+
+[[ $_install ]] && install
+[[ $_remove ]] && remove

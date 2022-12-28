7166e4b87133c11ed3e790b7e46d6d12fe8bd667
Carlo Lobrano <carlo@localhost-live.home-life.hub>
2022-12-28 11:44:49 +0100

    environments: add Telegram to flatpak apps

--- .config/cconf/environments/flatpak-install-apps.sh
+++ .config/cconf/environments/flatpak-install-apps.sh
@@ -3,5 +3,6 @@
 set -x
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub com.nextcloud.desktopclient.nextcloud
+#flatpak install flathub com.nextcloud.desktopclient.nextcloud
 flatpak install flathub md.obsidian.Obsidian
+flatpak install flathub org.telegram.desktop

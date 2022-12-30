fad7ec8412b80ad61c3b8356affab64a866a04a0
Carlo Lobrano <c.lobrano@gmail.com>
2022-12-30 11:28:44 +0100

    env: add support to konsole in dracula script

--- .config/cconf/environments/dracula-term-install-and-setup.sh
+++ .config/cconf/environments/dracula-term-install-and-setup.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 # -*- coding: UTF-8 -*-
 #sudo apt-get install dconf-cli
-git clone https://github.com/dracula/gnome-terminal ~/workspace/toolkit/dracula-gnome-terminal
-pushd ~/workspace/toolkit/dracula-gnome-terminal
-./install.sh
-popd
+
+if command -v gnome-terminal; then
+    git clone https://github.com/dracula/gnome-terminal ~/workspace/toolkit/dracula-gnome-terminal
+    pushd ~/workspace/toolkit/dracula-gnome-terminal
+    ./install.sh
+    popd
+fi
+
+if command -v konsole; then
+    wget https://github.com/dracula/konsole/archive/master.zip
+    unzip master.zip
+    cp konsole-master/Dracula.colorscheme ~/.local/share/konsole/
+    cat <<EOF
+    Now go to Konsole > Settings > Edit Current Profile… > Appearance tab
+    Select the Dracula scheme from the Color Schemes & Background… pane
+EOF
+    rm -r konsole-master master.zip
+fi

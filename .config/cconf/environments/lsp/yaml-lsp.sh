0c3fcea2029b993ef89bfabcfac14dffc1d24ad1
Carlo Lobrano <c.lobrano@gmail.com>
2023-01-10 11:54:18 +0100

    nvim: add YAML lsp

---
+++ .config/cconf/environments/lsp/yaml-lsp.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# -*- coding: UTF-8 -*-
+sudo dnf install -y yarnpkg
+sudo yarn global add yaml-language-server
--- .config/nvim/lua/nvim-lspconfig.lua
+++ .config/nvim/lua/nvim-lspconfig.lua
@@ -29,5 +29,12 @@ lspconfig.pylsp.setup{
   }
 }
 
-
-
+lspconfig.yamlls.setup({
+  settings = {
+    yaml = {
+      schemas = {
+        ["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] = "/*.k8s.yaml",
+      },
+    },
+  }
+})

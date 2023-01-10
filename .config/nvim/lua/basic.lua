fd541f4e37ee2ae15cc37c41393816acd6786d1c
Carlo Lobrano <c.lobrano@gmail.com>
2023-01-10 11:56:27 +0100

    nvim: replace vim.lsp.display with vim.lsp.show

--- .config/nvim/lua/basic.lua
+++ .config/nvim/lua/basic.lua
@@ -2,12 +2,7 @@ virtual_text = {}
 virtual_text.show = true
 virtual_text.toggle = function()
     virtual_text.show = not virtual_text.show
-    vim.lsp.diagnostic.display(
-    vim.lsp.diagnostic.get(0, 1),
-    0,
-    1,
-    {virtual_text = virtual_text.show}
-    )
+    vim.lsp.diagnostic.show(vim.lsp.diagnostic.get(0, 1), 0, 1, {virtual_text = virtual_text.show})
 end
 
 vim.api.nvim_set_keymap(

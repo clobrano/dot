69185c207be1e2da0b2b8f99e73330f7247c5c1e
Carlo Lobrano <c.lobrano@gmail.com>
2023-01-10 11:52:19 +0100

    tmux: letsdo: strip whitespaces

--- .config/cconf/tmux/plugins/letsdo.sh
+++ .config/cconf/tmux/plugins/letsdo.sh
@@ -2,23 +2,25 @@
 # -*- coding: UTF-8 -*-
 ## Read current Letsdo task title from tmux
 
+shopt -s extglob  # bash only
+
 # No Letsdo configuration
 [ ! -f $HOME/.letsdo ] && exit 0
 
 DATA_DIRECTORY=$(sed -n 's/DATA_DIRECTORY:\s*\(.*\)/\1/p' "$HOME/.letsdo")
 
 # no running tasks
-[ ! -f  "$DATA_DIRECTORY/letsdo-task" ] && exit 0
+[ ! -f  "$DATA_DIRECTORY/letsdo-task" ] && echo "No task running" && exit 0
 
 task=$(sed -n 's_"name": "\(.*\)",_\1_p' "$DATA_DIRECTORY/letsdo-task")
+task=${task##+([[:space:]])}    # strip leading whitespace;  no quote expansion!
+task=${task%%+([[:space:]])}   # strip trailing whitespace; no quote expansion!
+
 task_len=${#task}
 if [[ $task_len > 30 ]]; then
     task=${task:0:27}...
-else
-    # preserve the space around task name.
-    task="${task} "
 fi
 begin=$(date +%s -d "$(sed -n 's_"start": "\(.*\)"_\1_p' "$DATA_DIRECTORY/letsdo-task")")
 end=$(date +%s)
 # Why I need an 1h offset to get the right value? Is it for the daylight setting?
-echo " $task$(date +"%kh:%Mm" --date="@$(($end - $begin - 3600))")"
+echo "$task$(date +"%kh:%Mm" --date="@$(($end - $begin - 3600))")"

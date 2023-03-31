290233345f50d98de6d49eda9e41abe893670267
Carlo Lobrano <c.lobrano@gmail.com>
2023-03-31 10:18:02 +0200

    tmux: show kube config filename
    Previous kube config info was not descriptive enough. Now that I use
    symlink to config file, the name of the config file is much more
    descriptive.
    
    Signed-off-by: Carlo Lobrano <c.lobrano@gmail.com>

--- .config/cconf/tmux/plugins/kube_prompt.sh
+++ .config/cconf/tmux/plugins/kube_prompt.sh
@@ -1,19 +1,8 @@
 #!/usr/bin/env bash
 # -*- coding: UTF-8 -*-
 function zsh_kube_prompt() {
-    if ! command -v kubectl 2>&1 >/dev/null; then
-        return
-    fi
-    context=$(kubectl config current-context 2>/dev/null)
-    if [[ -z $context ]]; then
-        return
-    fi
-    cluster="$(kubectl config view -o "jsonpath={.contexts[?(@.name==\"$context\")].context.cluster}")"
-    if [[ -n $cluster ]]; then
-        printf "cluster:%s " ${cluster}
-    else
-        printf ""
-    fi
+    # Keep the space at the end of the printf message
+    printf "$(basename `readlink $HOME/.kube/config` | awk -F"." '{print $1"."$2}') "
 }
 
 zsh_kube_prompt

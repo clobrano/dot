dcc85593e9ec141bc30aadc1db62535a174fbd76
Carlo Lobrano <c.lobrano@gmail.com>
2023-07-08 22:07:05 +0200

    environments: update flatpak selection to install
    Signed-off-by: Carlo Lobrano <c.lobrano@gmail.com>

--- .config/cconf/environments/flatpak-install-apps.sh
+++ .config/cconf/environments/flatpak-install-apps.sh
@@ -4,6 +4,18 @@ set -x
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 #flatpak install flathub com.nextcloud.desktopclient.nextcloud
-flatpak install flathub md.obsidian.Obsidian
-flatpak install flathub org.telegram.desktop
-flatpak install com.ticktick.TickTick
+
+selection=(
+    com.dropbox.Client
+    com.github.tchx84.Flatseal
+    com.google.Chrome
+    com.raggesilver.BlackBox
+    md.obsidian.Obsidian
+    org.gnome.gitlab.somas.Apostrophe 
+    org.telegram.desktop
+)
+
+
+for package in ${selection[@]}; do
+    flatpak install --assumeyes ${package}
+done

2a45976a1484ab3096c937128a78a63f8546da96
Carlo Lobrano <c.lobrano@gmail.com>
2023-08-05 10:55:33 +0200

    ranger: force leader-f to Nop

--- .config/nvim/lua/plugins/ranger.lua
+++ .config/nvim/lua/plugins/ranger.lua
@@ -4,5 +4,6 @@ let g:ranger_map_keys = 0
 let g:ranger_replace_netrw = 1
 ]]
 
+vim.keymap.set('n', '<leader>f', '<Nop>') -- trying to disable annoying <leader>f mapping
 vim.keymap.set('n', '<leader>rr', ':Ranger<cr>', { silent = true })
 vim.keymap.set('n', '<leader>rw', ':RangerWorkingDirectory', { silent = true })

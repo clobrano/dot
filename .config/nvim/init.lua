f10117665de0574cd517b7dd9e8128dc26b9c1af
Carlo Lobrano <c.lobrano@gmail.com>
2023-08-05 11:58:51 +0200

    Add style to LSP floating windows
    Signed-off-by: Carlo Lobrano <c.lobrano@gmail.com>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -344,20 +344,26 @@ local on_attach = function(_, bufnr)
   end, { desc = 'Format current buffer with LSP' })
 end
 
--- vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
---     vim.lsp.diagnostic.on_publish_diagnostics, {
---         virtual_text = false,
---         underline = false,
---         signs = true,
---         update_in_insert = true,
---     }
--- )
--- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
---     vim.lsp.handlers.hover, {
---         -- Use a sharp border with `FloatBorder` highlights
---         border = "single"
---     }
--- )
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    virtual_text = false,
+    underline = false,
+    signs = true,
+    update_in_insert = true,
+  }
+)
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover, {
+    -- Use a sharp border with `FloatBorder` highlights
+    border = "single"
+  }
+)
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+  vim.lsp.handlers.hover, {
+    -- Use a sharp border with `FloatBorder` highlights
+    border = "single"
+  }
+)
 
 -- Enable the following language servers
 --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.

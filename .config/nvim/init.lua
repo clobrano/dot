f8e52caf9e298a6559049beef934ab28026ab25f
Carlo Lobrano <c.lobrano@gmail.com>
2023-08-05 10:53:44 +0200

    Add more cmp completion sources

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -109,6 +109,9 @@ require('lazy').setup({
       'L3MON4D3/LuaSnip', -- Snippet Engine & its associated nvim-cmp source
       'saadparwaiz1/cmp_luasnip',
       'hrsh7th/cmp-nvim-lsp', -- Adds LSP completion capabilities
+      'hrsh7th/cmp-cmdline', -- Source for vim's cmdline
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
       'rafamadriz/friendly-snippets', -- Adds a number of user-friendly snippets
     },
   },
@@ -429,6 +432,8 @@ cmp.setup {
   sources = {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
+    { name = 'path' },
+    { name = 'buffer' },
   },
   experimental = {
         ghost_text = true

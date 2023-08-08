4abea4e7315e58160fb63c813eda68bb6a6eb1a7
Carlo Lobrano <c.lobrano@gmail.com>
2023-08-08 16:49:16 +0200

    Fix telescope double settings
    Signed-off-by: Carlo Lobrano <c.lobrano@gmail.com>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -216,47 +216,26 @@ require("indent_blankline").setup {
 
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
-require('telescope').setup{
-    defaults = {
-        layout_strategy = 'vertical',
-        layout_config = {
-            width = 0.95,
-            height = 0.95,
-            horizontal = {preview_width = 0.5},
-            vertical = {preview_height = 0.7},
-        },
-        file_ignore_patterns = {
-            "^.git/", "node_modules/", "^vendor/"
-        },
+require('telescope').setup {
+  defaults = {
+    layout_strategy = 'vertical',
+    --layout_config = {
+      --width = 0.95,
+      --height = 0.95,
+      --horizontal = { preview_width = 0.5 },
+      --vertical = { preview_height = 0.7 },
+    --},
+    file_ignore_patterns = {
+      "^.git/", "node_modules/", "^vendor/"
     },
-    pickers = {
-        find_files = {
-            no_ignore = true,
-        }
-    },
-    config = function()
-        vim.cmd[[
-            " Mapping
-            nnoremap <leader>fa :Telescope live_grep<cr>
-            nnoremap <leader>fb :Telescope buffers<cr>
-            nnoremap <leader>fd :Telescope lsp_definitions<cr>
-            nnoremap <leader>ff :Telescope find_files hidden=true<cr>
-            nnoremap <leader>fg :Telescope current_buffer_fuzzy_find<cr>
-            nnoremap <leader>fh :Telescope help_tags<cr>
-            nnoremap <leader>fk :Telescope keymaps<cr>
-            nnoremap <leader>fi :Telescope lsp_implementations<cr>
-            nnoremap <leader>fl :Telescope resume<cr>
-            nnoremap <leader>fm :Telescope man_pages<cr>
-            nnoremap <leader>fr :Telescope lsp_references<cr>
-            nnoremap <leader>fs :Telescope grep_string<cr>
-            nnoremap <leader>ft :Telescope tags<cr>
-
-            " GIT mappings
-            nnoremap <leader>fgb :Telescope git_branches<cr>
-            nnoremap <leader>fgc :Telescope git_commits<cr>
-            nnoremap <leader>fgs :Telescope git_stashes<cr>
-        ]]
-    end,
+  },
+  pickers = {
+    find_files = {
+      no_ignore = true,
+    }
+  },
+  config = function()
+  end,
 }
 
 -- Enable telescope fzf native, if installed
@@ -274,7 +253,6 @@ end, { desc = '[/] Fuzzily search in current buffer' })
 vim.keymap.set('n', '<leader>fa', require('telescope.builtin').live_grep, { desc = '[F]ind [A]all' })
 vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers, { desc = '[F]ind [B]uffers' })
 vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[F]ind [F]iles' })
-vim.keymap.set('n', '<leader>fg', require('telescope.builtin').current_buffer_fuzzy_find, { desc = '[F]ind [G]rep current buffer' })
 vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = '[F]ind [H]elp' })
 vim.keymap.set('n', '<leader>fk', require('telescope.builtin').keymaps, { desc = '[F]ind [k]eymaps' })
 vim.keymap.set('n', '<leader>fl', require('telescope.builtin').resume, { desc = '[F]ind [L]ast search' })
@@ -516,13 +494,34 @@ cmp.setup {
   sources = {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
-    { name = 'path' },
-    { name = 'buffer' },
   },
   experimental = {
     ghost_text = false
   },
 }
+-- `/` cmdline setup.
+cmp.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+-- `:` cmdline setup.
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    {
+      name = 'cmdline',
+      option = {
+        ignore_cmds = { 'Man', '!' }
+      }
+    }
+  })
+})
+
+vim.keymap.set('n', '<leader>ai', ':NeoAIContext<cr>')
 
 -- Neovide custom settings
 if vim.fn.exists('g:neovide') == 1 then

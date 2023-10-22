3c70af53135ba1c0ad11118e2770aa60589c2b5f
Carlo Lobrano <c.lobrano@gmail.com>
2023-10-22 18:01:27 +0200

    nvim: add spell active status in statusline
    Signed-off-by: Carlo Lobrano <c.lobrano@gmail.com>

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,3 +1,10 @@
+local function spell()
+  if vim.wo.spell == true then -- Note that 'spell' is a window option, so: wo
+    return '[' .. vim.bo.spelllang .. ']'
+  end
+  return ''
+end
+
 return {
     'nvim-lualine/lualine.nvim',
     opts = {
@@ -6,10 +13,10 @@ return {
             theme = 'auto',
         },
         sections = {
-            --lualine_a = {'clobrano#status#workingdirectory'},
-            lualine_b = {'branch'},
-            lualine_c = { {'filename', path = 1} },
-            lualine_x = {'diff', 'diagnostics', 'filetype', 'fileformat', 'encoding'},
+            lualine_a = {'branch'},
+            lualine_b = { {'filename', path = 1} },
+            lualine_c = {},
+            lualine_x = {'diff', 'diagnostics', spell, 'filetype', 'fileformat', 'encoding'},
             lualine_y = {'progress', 'searchcount'},
             lualine_z = {'location'}
         },

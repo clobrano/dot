1633503f46f9e671413caccd9ebd49bda0b9fbd3
Carlo Lobrano <c.lobrano@gmail.com>
2023-12-17 09:34:37 +0100

    env: add swgo function to update go current version in shell
    Signed-off-by: Carlo Lobrano <c.lobrano@gmail.com>

--- .config/cconf/zsh/functions.zsh
+++ .config/cconf/zsh/functions.zsh
@@ -39,6 +39,10 @@ function switchGoVersion() {
     fi
 }
 
+function swgo() {
+    switchGoVersion ./go.mod
+}
+
 function fd() {
   preview="git diff $@ --color=always -- {-1}"
   git diff $@ --name-only | fzf -m --ansi --preview $preview

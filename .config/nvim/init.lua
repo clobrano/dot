50b595b11cf16016c8b2706c5018436a91627cee
Carlo Lobrano <c.lobrano@gmail.com>
2024-01-28 10:06:13 +0100

    nvim: Gen refresh config
    Signed-off-by: Carlo Lobrano <c.lobrano@gmail.com>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -189,10 +189,10 @@ local plugins = {
     "David-Kunz/gen.nvim",
     opts = {
       model = "mistral",      -- The default model to use.
-      display_mode = "float", -- The display mode. Can be "float" or "split".
+      display_mode = "split", -- The display mode. Can be "float" or "split".
       show_prompt = false,    -- Shows the Prompt submitted to Ollama.
       show_model = true,      -- Displays which model you are using at the beginning of your chat session.
-      no_auto_close = false,  -- Never closes the window automatically.
+      no_auto_close = true,  -- Never closes the window automatically.
       init = function(options) pcall(io.popen, "ollama serve > /dev/null 2>&1 &") end,
       -- Function to initialize Ollama
       command = "curl --silent --no-buffer -X POST http://localhost:11434/api/generate -d $body",

429fc30094d52897216b653de40fcce7316be551
Carlo Lobrano <c.lobrano@gmail.com>
2024-02-02 22:40:09 +0100

    Ensure flatpak is installed
    Signed-off-by: Carlo Lobrano <c.lobrano@gmail.com>

--- .config/cconf/environments/flatpak-install-apps.sh
+++ .config/cconf/environments/flatpak-install-apps.sh
@@ -2,6 +2,10 @@
 # -*- coding: UTF-8 -*-
 set -x
 
+if !command -v flatpak 2>/dev/null; then
+    sudo dnf install -y flatpak
+fi
+
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 #flatpak install flathub com.nextcloud.desktopclient.nextcloud
 

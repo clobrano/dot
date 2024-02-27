ab2969766de5bddbe31670e186d6562ad8a35b3c
Carlo Lobrano <c.lobrano@gmail.com>
2024-02-27 08:47:42 +0100

    nvim: update startify configuration
    - Commands to open sessions
    - Last commits from git repos
    
    Signed-off-by: Carlo Lobrano <c.lobrano@gmail.com>

--- .config/nvim/lua/plugins/startify.lua
+++ .config/nvim/lua/plugins/startify.lua
@@ -1,8 +1,8 @@
 return {
     'mhinz/vim-startify',
-    dependencies = {'nvim-tree/nvim-web-devicons'},
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-        vim.cmd[[
+        vim.cmd [[
             cnoreabbrev ss Startify
 
             let g:startify_custom_header =
@@ -11,8 +11,10 @@ return {
             let g:startify_change_to_dir=1
             let g:startify_change_to_vcs_root = 1
             let g:startify_commands = [
-            \ { 'I': ['Open init.lua', 'e ~/.dot/.config/nvim/init.lua| lcd %:p:h'] }
-            \ ]
+                \ { 'i': ['Open init.lua', 'e ~/.dot/.config/nvim/init.lua| lcd %:p:h'] },
+                \ { 'n': ['Open Notes', 'SLoad RedHatVault'] },
+                \ { 'e': ['Open Espanso', 'SLoad Espanso'] },
+                \ ]
             let g:startify_enable_special = 0
             let g:startify_files_number = 3
 
@@ -20,16 +22,18 @@ return {
             let g:startify_session_delete_buffers = 1
             let g:startify_session_persistence = 1
 
-            let g:startify_lists = [
-            \ { 'type': 'commands',  'header': ['   Commands']       },
-            \ { 'type': 'dir',       'header': ['   MRU in '. getcwd()] },
-            \ { 'type': 'sessions',  'header': ['   Sessions']       },
-            \ ]
+            function! s:list_commits()
+                let git = 'git '
+                let commits = systemlist(git .' log --oneline | head -n10')
+                let git = 'G'. git[1:]
+                return map(commits, '{"line": matchstr(v:val, "\\s\\zs.*"), "cmd": "'. git .' show ". matchstr(v:val, "^\\x\\+") }')
+            endfunction
 
-            "function! StartifyEntryFormat()
-            "    return 'WebDevIconsGetFileTypeSymbol(absolute_path) ." ". entry_path'
-            "endfunction
+            let g:startify_lists = [
+                \ { 'type': 'commands',  'header': ['   Commands']       },
+                \ { 'header': ['   Recent files in this directory'], 'type': 'dir' },
+                \ { 'header': ['   Commits'],        'type': function('s:list_commits') },
+                \ ]
         ]]
     end,
 }
-

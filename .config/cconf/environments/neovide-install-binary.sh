e025f6c123f330a273e01d3cf2810406cead6dde
Carlo Lobrano <c.lobrano@gmail.com>
2024-06-04 23:17:45 +0200

    create local bin directory if it does not exist
    Signed-off-by: Carlo Lobrano <c.lobrano@gmail.com>

--- .config/cconf/environments/neovide-install-binary.sh
+++ .config/cconf/environments/neovide-install-binary.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 # -*- coding: UTF-8 -*-
 VERSION=$(curl --silent "https://api.github.com/repos/neovide/neovide/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
-echo "Installing neovide version $VERSION"
+echo "Installing neovide version $VERSION (press ENTER to continue)"
+read
 
 wget https://github.com/neovide/neovide/releases/download/${VERSION}/neovide-linux-x86_64.tar.gz  -O /tmp/neovide.tar.gz
+mkdir -p $HOME/.local/bin
 tar xvf /tmp/neovide.tar.gz --directory $HOME/.local/bin
 chmod u+x $HOME/.local/bin/neovide 
 

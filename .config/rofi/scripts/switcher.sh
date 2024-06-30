79b40ce6f0665ef5f6b52257cc04b309445f93f6
Carlo Lobrano <c.lobrano@gmail.com>
2024-06-30 17:56:27 +0200

    hyprland: task switcher with rofi

--- .config/rofi/scripts/switcher.sh
+++ .config/rofi/scripts/switcher.sh
@@ -5,11 +5,10 @@ windows=$(hyprctl -j clients)
 active_address=$(hyprctl -j activewindow | jq -r '.address')
 
 selected=$(echo $windows | \
-    jq -r '.[] | "\(.initialTitle) |\(.address)"' | \
+    jq -r '.[] | "\(.initialTitle) (\(.title)) |\(.address)"' | \
     sed "s|$active_address|focused|" | \
     rofi -dmenu -p "" -i -normal-window)
 
-set -x
 addr="$(echo "$selected" | awk -F '|' '{print $2}')"
 
 hyprctl dispatch focuswindow address:$addr

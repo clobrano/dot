cdfdf5486b50146ba241eb906b2e6d0f1d27f2ab
Carlo Lobrano <c.lobrano@gmail.com>
2024-07-21 10:32:19 +0200

    Add taskwarrior integration for tmux

---
+++ .config/cconf/tmux/plugins/taskwarrior.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# -*- coding: UTF-8 -*-
+
+DUE_TODAY=$(task +PENDING +TODAY count)
+OVERDUE=$(task +OVERDUE count)
+DUE_THIS_WEEK=$(task +PENDING due.after=sow due.before=eow count)
+ACTIVE=$(task +ACTIVE count)
+# `count` command doesn't seem to work with `completed` items
+DONE_THIS_WEEK=$(task completed end.after=sow end.before=eow | grep -E "^ -" -c)
+
+echo 󰻌 ${OVERDUE} 󱄻 ${DUE_TODAY} 󰫚 ${ACTIVE} ﮹ ${DUE_THIS_WEEK} 󰕥 ${DONE_THIS_WEEK}

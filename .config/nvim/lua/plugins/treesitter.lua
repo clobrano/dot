b164b9e822930e47b34dc554a7b94dd68f6d635d
Carlo Lobrano <c.lobrano@gmail.com>
2024-07-21 10:35:35 +0200

    nvim:treesitter: add yaml

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return { -- Highlight, edit, and navigate code
   config = function()
     require('nvim-treesitter.configs').setup {
       -- Add languages to be installed here that you want installed for treesitter
-      ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'vimdoc', 'vim', 'markdown' },
+      ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'vimdoc', 'vim', 'markdown', 'yaml' },
 
       -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
       auto_install = false,

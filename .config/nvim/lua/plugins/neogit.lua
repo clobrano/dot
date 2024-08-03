4d6036f8c93480cb8be8d87e4f33bf1dc163ec43
Carlo Lobrano <c.lobrano@gmail.com>
2024-08-03 15:29:23 +0200

    nvim:neogit: formatting

--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -1,12 +1,19 @@
 return {
-    "NeogitOrg/neogit",
-    dependencies = {
-        "nvim-lua/plenary.nvim", -- required
-        "sindrets/diffview.nvim", -- optional - Diff integration
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",      -- required
+    "sindrets/diffview.nvim",     -- optional - Diff integration
 
-        -- Only one of these is needed, not both.
-        "nvim-telescope/telescope.nvim", -- optional
-        "ibhagwan/fzf-lua",            -- optional
-    },
-    config = true
+    -- Only one of these is needed, not both.
+    "nvim-telescope/telescope.nvim",     -- optional
+    "ibhagwan/fzf-lua",                  -- optional
+  },
+  --config = true,
+  config = function()
+    require('neogit').setup{
+      status = {
+        recent_commit_count = 15,
+      }
+    }
+  end
 }

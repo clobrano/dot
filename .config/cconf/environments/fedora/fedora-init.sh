2640d55aebd5d5bef2a59ac4af20e297f26b13b6
Carlo Lobrano <c.lobrano@gmail.com>
2024-08-03 15:32:04 +0200

    env: update fedora scripts

--- .config/cconf/environments/fedora/fedora-init.sh
+++ .config/cconf/environments/fedora/fedora-init.sh
@@ -5,7 +5,7 @@ ENVIRONMENT=$HOME/.dot/.config/cconf/environments
 pushd $HOME/.dot
 set -x
 git submodule update --init
-$ENVIRONMENT/fedora/dnf-speed-up.sh
+$ENVIRONMENT/fedora/fedora-dnf-speed-up.sh
 $ENVIRONMENT/fedora/dnf-install-base-requirements.sh
 $ENVIRONMENT/fedora/fedora-enable-rpm-fusion.sh
 $ENVIRONMENT/fedora/fedora-install-media-codecs.sh

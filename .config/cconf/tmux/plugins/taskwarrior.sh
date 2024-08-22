2390db6d0fd3b4249a122bd3d362aeeafc1750ec
Carlo Lobrano <c.lobrano@gmail.com>
2024-08-22 09:49:59 +0200

    tmux:taskwarrior: better handling of errors

--- .config/cconf/tmux/plugins/taskwarrior.sh
+++ .config/cconf/tmux/plugins/taskwarrior.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 # -*- coding: UTF-8 -*-
 
-DUE_TODAY=$(task +PENDING +TODAY count)
-OVERDUE=$(task +OVERDUE count)
-DUE_THIS_WEEK=$(task +PENDING due.after=sow due.before=eow count)
-ACTIVE=$(task +ACTIVE count)
+# Use default taskwarrior binary if not set externally
+: ${TASK:="task rc:~/.taskworkrc"}
+DUE_TODAY=$(${TASK} +PENDING +TODAY count)
+[ $? -ne 0 ] && cat ~/.taskwarrior-stats && exit 0
+OVERDUE=$(${TASK} +OVERDUE count)
+[ $? -ne 0 ] && cat ~/.taskwarrior-stats && exit 0
+DUE_THIS_WEEK=$(${TASK} +PENDING due.after=sow due.before=eow count)
+[ $? -ne 0 ] && cat ~/.taskwarrior-stats && exit 0
+ACTIVE=$(${TASK} +ACTIVE count)
+[ $? -ne 0 ] && cat ~/.taskwarrior-stats && exit 0
 # `count` command doesn't seem to work with `completed` items
-DONE_THIS_WEEK=$(task completed end.after=sow end.before=eow | grep -E "^ -" -c)
+DONE_THIS_WEEK=$(${TASK} completed end.after=sow end.before=eow | grep -E "^ -" -c)
+[ $? -ne 0 ] && cat ~/.taskwarrior-stats && exit 0
 
-echo 󰻌 ${OVERDUE} 󱄻 ${DUE_TODAY} 󰫚 ${ACTIVE} 󰚻 ${DUE_THIS_WEEK} 󰕥 ${DONE_THIS_WEEK}
+echo 󰻌 ${OVERDUE:-?} 󱄻 ${DUE_TODAY:-?} 󰫚 ${ACTIVE:-?} 󰚻 ${DUE_THIS_WEEK:-?} 󰕥 ${DONE_THIS_WEEK:-?} | tee ~/.taskwarrior-stats

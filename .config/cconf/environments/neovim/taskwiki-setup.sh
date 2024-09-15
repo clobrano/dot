3792bd1c6f0577ba8f1a8839ee86e32a3cd7083b
Carlo Lobrano <c.lobrano@gmail.com>
2024-09-15 08:36:08 +0000

    sync with master branch

---
+++ .config/cconf/environments/neovim/taskwiki-setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# -*- coding: UTF-8 -*-
+# Python support in neovim
+pip install pynvim
+# Taskwarrior
+pip install tasklib
+## for installing integration with google (e.g. Google Keep / Calendar) and Notion
+pip3 install "syncall[google,tw]"

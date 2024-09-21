f7a654954368a1104a9a466726616264952464bd
Carlo Lobrano <c.lobrano@gmail.com>
2024-09-21 10:16:47 +0200

    nvim:icon-picker: delete re-mapping of <C-t>

--- .config/nvim/lua/plugins/icon-picker.lua
+++ .config/nvim/lua/plugins/icon-picker.lua
@@ -7,6 +7,5 @@ return {
 
         vim.keymap.set("n", "<Leader><Leader>i", "<cmd>IconPickerNormal<cr>", opts)
         vim.keymap.set("n", "<Leader><Leader>y", "<cmd>IconPickerYank<cr>", opts) --> Yank the selected icon into register
-        vim.keymap.set("i", "<C-t>", "<cmd>IconPickerInsert<cr>", opts)
     end
 }

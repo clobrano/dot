5c5031aff5d0eb47675333af07c10a12e0814d77
Carlo Lobrano <c.lobrano@gmail.com>
2024-10-02 17:51:33 +0200

    nvim:render-markdown: improved code style render

--- .config/nvim/lua/plugins/render-markdown.lua
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -37,11 +37,11 @@ return {
         -- Determins if a border is added above and below headings
         border = false,
         -- Highlight the start of the border using the foreground highlight
-        border_prefix = false,
+        border_prefix = true,
         -- Used above heading for border
-        above = '▄',
+        above = '',
         -- Used below heading for border
-        below = '▀',
+        below = '',
         -- The 'level' is used to index into the array using a clamp
         -- Highlight for the heading icon and extends through the entire line
         backgrounds = {
@@ -81,6 +81,56 @@ return {
             prog50 = { raw = '[o]', rendered = '󰄱 ', highlight = 'RenderMarkdownTodo' },
             prog75 = { raw = '[O]', rendered = '󰄱 ', highlight = 'RenderMarkdownTodo' },
         },
+    },
+    code = {
+        -- Turn on / off code block & inline code rendering
+        enabled = true,
+        -- Turn on / off any sign column related rendering
+        sign = true,
+        -- Determines how code blocks & inline code are rendered:
+        --  none:     disables all rendering
+        --  normal:   adds highlight group to code blocks & inline code, adds padding to code blocks
+        --  language: adds language icon to sign column if enabled and icon + name above code blocks
+        --  full:     normal + language
+        style = 'normal',
+        -- Determines where language icon is rendered:
+        --  right: right side of code block
+        --  left:  left side of code block
+        position = 'left',
+        -- Amount of padding to add around the language
+        -- If a floating point value < 1 is provided it is treated as a percentage of the available window space
+        language_pad = 0,
+        -- A list of language names for which background highlighting will be disabled
+        -- Likely because that language has background highlights itself
+        disable_background = { 'diff' },
+        -- Width of the code block background:
+        --  block: width of the code block
+        --  full:  full width of the window
+        width = 'block',
+        -- Amount of margin to add to the left of code blocks
+        -- If a floating point value < 1 is provided it is treated as a percentage of the available window space
+        -- Margin available space is computed after accounting for padding
+        left_margin = 0,
+        -- Amount of padding to add to the left of code blocks
+        -- If a floating point value < 1 is provided it is treated as a percentage of the available window space
+        left_pad = 0,
+        -- Amount of padding to add to the right of code blocks when width is 'block'
+        -- If a floating point value < 1 is provided it is treated as a percentage of the available window space
+        right_pad = 0,
+        -- Minimum width to use for code blocks when width is 'block'
+        min_width = 0,
+        -- Determins how the top / bottom of code block are rendered:
+        --  thick: use the same highlight as the code body
+        --  thin:  when lines are empty overlay the above & below icons
+        border = 'thick',
+        -- Used above code blocks for thin border
+        above = '▄',
+        -- Used below code blocks for thin border
+        below = '▀',
+        -- Highlight for code blocks
+        highlight = 'RenderMarkdownCode',
+        -- Highlight for inline code
+        highlight_inline = 'RenderMarkdownCodeInline',
     },
     })
   end

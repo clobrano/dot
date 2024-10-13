3cff7417e624af415a82471b729c4f4464c2e54c
Carlo Lobrano <c.lobrano@gmail.com>
2024-10-13 10:42:17 +0200

    nvim:taskwiki: shortcut buffer reload

--- .config/nvim/lua/plugins/taskwiki.lua
+++ .config/nvim/lua/plugins/taskwiki.lua
@@ -5,5 +5,6 @@ return {
 		let g:taskwiki_extra_warriors={ 'W': {'data_location': '/home/clobrano/Documents/taskwarriorRH/', 'taskrc_location': '/home/clobrano/.taskworkrc'}, 'P': {'data_location': '/home/clobrano/Me/Taskwarrior/', 'taskrc_location': '/home/clobrano/.taskrc'}}
         let g:taskwiki_disable_concealcursor="yes"
     ]]
+	vim.keymap.set('n', '<leader>tr', ':TaskWikiBufferLoad<cr>')
 	end
 }

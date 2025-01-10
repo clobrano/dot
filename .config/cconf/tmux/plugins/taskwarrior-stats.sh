1b586bf59485c84d99f3eb8251dab79ed326e9b2
Carlo Lobrano <c.lobrano@gmail.com>
2025-01-10 09:47:44 +0100

    tmux:taskwarrior: avoid presenting partial information on tmux output
    sometimes updating taskwarrior stats fails
    
    Signed-off-by: Carlo Lobrano <c.lobrano@gmail.com>

--- .config/cconf/tmux/plugins/taskwarrior-stats.sh
+++ .config/cconf/tmux/plugins/taskwarrior-stats.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 # -*- coding: UTF-8 -*-
-interval_time_min=2
+interval_time_min=5
 current_time_min=$(date +%M)
 if [ $((current_time_min % interval_time_min)) -eq 0 ]; then
     "$HOME"/workspace/script-fu/taskwarrior-update-stats.sh
+fi
+# sometimes updating taskwarrior stats fails. Avoid presenting
+# partial information on tmux output
+if ! grep "?" "$HOME"/.taskwarrior-stats >/dev/null; then
     cat "$HOME"/.taskwarrior-stats
 fi
+
 
 reminder_interval_time_min=10
 if [ $((current_time_min % reminder_interval_time_min)) -eq 0 ]; then

feb4204222897f95c03cd0fa7c5eceb975591eaf
Carlo Lobrano <c.lobrano@gmail.com>
2025-01-10 09:43:24 +0100

    tmux:taskwarrior: Fix reminder interval

--- .config/cconf/tmux/plugins/taskwarrior-stats.sh
+++ .config/cconf/tmux/plugins/taskwarrior-stats.sh
@@ -3,11 +3,11 @@
 interval_time_min=2
 current_time_min=$(date +%M)
 if [ $((current_time_min % interval_time_min)) -eq 0 ]; then
-    $HOME/workspace/script-fu/taskwarrior-update-stats.sh
-    cat /home/clobrano/.taskwarrior-stats
+    "$HOME"/workspace/script-fu/taskwarrior-update-stats.sh
+    cat "$HOME"/.taskwarrior-stats
 fi
 
 reminder_interval_time_min=10
-if [ $((current_time_min % interval_time_min)) -eq 0 ]; then
-    $HOME/workspace/script-fu/taskwarrior-reminder.sh
+if [ $((current_time_min % reminder_interval_time_min)) -eq 0 ]; then
+    "$HOME"/workspace/script-fu/taskwarrior-reminder.sh
 fi

85967cda205163d27c6c279ca08e85ca850c61b4
Carlo Lobrano <c.lobrano@gmail.com>
2025-04-24 18:41:32 +0200

    taskwarrior: update UI and configuration

--- .config/cconf/environments/taskwarrior/taskwarrior-install-and-setup.sh
+++ .config/cconf/environments/taskwarrior/taskwarrior-install-and-setup.sh
@@ -1,33 +1,53 @@
 #!/usr/bin/env bash
 # -*- coding: UTF-8 -*-
 
-echo "installing tasklib..."
-pip3 install --user tasklib
-pip3 install --user pynvim
+: "${TASKWARRIOR_VERSION:=3.3.0}"
+: "${TASKWARRIOR_TUI_VERSION:=v0.26.3}"
 
-echo "installing letsdo..."
-pip3 install --user letsdo
+install_dependencies() {
+    echo "[+] installing tasklib..."
+    pip3 install --user tasklib
+    pip3 install --user pynvim
+}
 
-set -x
-if [ ! -f task-3.3.0.tar.gz ]; then
-    if ! wget https://github.com/GothenburgBitFactory/taskwarrior/releases/download/v3.3.0/task-3.3.0.tar.gz; then
-        echo "[!] could not curl taskwarrior"
+install_taskwarrior_tui()
+{
+    local archive="taskwarrior-tui-x86_64-unknown-linux-gnu.tar.gz"
+    if ! wget "https://github.com/kdheepak/taskwarrior-tui/releases/download/$TASKWARRIOR_TUI_VERSION/$archive"; then
+        echo "[!] could not get taskwarrior-tui release $TASKWARRIOR_TUI_VERSION"
         exit 1
     fi
-fi
+    tar xvf "$archive"
+    mv taskwarrior-tui ~/.local/bin
+    rm -f "$archive"
+}
 
-sudo dnf install -y cmake libuuid-devel
-../rust-install-and-setup.sh
+build_from_source() {
+    local package="task-$TASKWARRIOR_VERSION"
+    local archive="$package.tar.gz"
 
-mkdir -p ~/Apps
-mv task-3.3.0.tar.gz ~/Apps
+    if [ ! -f "$archive" ]; then
+        if ! wget "https://github.com/GothenburgBitFactory/taskwarrior/releases/download/v$TASKWARRIOR_VERSION/$archive"; then
+            echo "[!] could not get taskwarrior release v$TASKWARRIOR_VERSION"
+            exit 1
+        fi
+    fi
 
-pushd ~/Apps || exit 1
-tar xvf task-3.3.0.tar.gz
-cd task-3.3.0
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
-cmake --build build
-sudo cmake --install build
-popd || exit 1
+    sudo dnf install -y cmake libuuid-devel
+    ../rust-install-and-setup.sh
 
-echo [+] done.
+    mkdir -p ~/Apps
+    mv "$archive" ~/Apps
+
+    pushd ~/Apps || exit 1
+    tar xvf "$archive"
+    pushd "$package" || exit 1
+    cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
+    cmake --build build
+    sudo cmake --install build
+    popd || exit 1
+    rm "$archive"
+    popd || exit 1
+
+    echo "[+] done."
+}

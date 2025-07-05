b9cb47f22cae981c2cf2504a4505aab4c3a69b6b
Carlo Lobrano <c.lobrano@gmail.com>
2025-07-05 15:55:41 +0200

    nvim: reconfigured telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,3 +1,67 @@
+local is_vendor_ignored = true
+local telescope_opts = {
+  defaults = {
+    file_ignore_patterns = {
+      "^.git/", "node_modules/", "^vendor/", "^venv/", "^.venv/", "^tags$"
+    },
+    path_display = Filename_first_path_display,
+    layout_strategy = 'flex',
+    layout_config = {
+      width = 0.99,
+      height = 0.99,
+      flex = {
+        -- Minimum width required for 'horizontal' layout (preview on the right)
+        -- If the window width is less than this, it will switch to 'vertical' (preview on top)
+        -- You'll need to adjust this value based on your screen size and preferences.
+        -- For example, 120 or 150 might be good starting points.
+        width = 0.99,        -- You can also use a fixed number like 150
+        height = 0.99,
+        preview_width = 0.8, -- Percentage of the available horizontal space for the preview window
+        -- You can further fine-tune the horizontal and vertical specific configurations
+        horizontal = {
+          prompt_position = "top",
+          preview_width = 0.7, -- Adjust results window width if needed
+        },
+        vertical = {
+          prompt_position = "top",
+          mirror = true,
+          -- The preview window will be above the results in this layout
+        },
+      },
+    },
+    pickers = {
+      lsp_references = { fname_width = 100, },
+      tags = { fname_width = 100, },
+      find_files = {
+        no_ignore = true,
+        no_ignore_parent = true,
+      },
+      colorscheme = {
+        enable_preview = true
+      },
+    },
+
+  },
+}
+
+_G.TelescopeToggleVendorIgnore = function()
+  is_vendor_ignored = not is_vendor_ignored
+  if is_vendor_ignored then
+    -- Include vendor golang directory in the excluded list
+    telescope_opts.defaults.file_ignore_patterns = {
+      "^.git/", "node_modules/", "^vendor/", "^venv/", "^.venv/", "^tags$"
+    }
+    print("Vendor folder is now EXCLUDED from Telescope search")
+  else
+    -- Remove vendor golang directory from the excluded list
+    telescope_opts.defaults.file_ignore_patterns = {
+      "^.git/", "node_modules/", "^venv/", "^.venv/", "^tags$"
+    }
+    print("Vendor folder is now INCLUDED in Telescope search")
+  end
+  require("telescope").setup(telescope_opts)
+end
+
 return {
   'nvim-telescope/telescope.nvim',
   branch = '0.1.x',
@@ -29,69 +93,7 @@ return {
       --return string.format("%s %s", tail, path)
     end
 
-    require('telescope').setup({
-      defaults = {
-        file_ignore_patterns = {
-          "^.git/", "node_modules/", "^vendor/", "^venv/", "^.venv/", "^tags$"
-        },
-        path_display = Filename_first_path_display,
-        mappings = {
-          i = {
-            ["<C-v>"] = require('telescope.actions').select_vertical, -- Map <C-v> to open in vertical split in insert mode
-          },
-          n = {
-            ["p"] = require('telescope_insert_path').insert_reltobufpath_insert,
-          },
-        },
-        layout_strategy = 'flex',
-        layout_config = {
-          width = 0.99,
-          height = 0.99,
-          flex = {
-            -- Minimum width required for 'horizontal' layout (preview on the right)
-            -- If the window width is less than this, it will switch to 'vertical' (preview on top)
-            -- You'll need to adjust this value based on your screen size and preferences.
-            -- For example, 120 or 150 might be good starting points.
-            width = 0.99,        -- You can also use a fixed number like 150
-            height = 0.99,
-            preview_width = 0.8, -- Percentage of the available horizontal space for the preview window
-            -- You can further fine-tune the horizontal and vertical specific configurations
-            horizontal = {
-              prompt_position = "top",
-              preview_width = 0.7, -- Adjust results window width if needed
-            },
-            vertical = {
-              prompt_position = "top",
-              mirror = true,
-              -- The preview window will be above the results in this layout
-            },
-          },
-        },
-        pickers = {
-          lsp_references = { fname_width = 100, },
-          tags = { fname_width = 100, },
-          find_files = {
-            no_ignore = true,
-            no_ignore_parent = true,
-            mappings = {
-              n = {
-                ["cd"] = function(prompt_bufnr)
-                  local selection = require("telescope.actions.state").get_selected_entry()
-                  local dir = vim.fn.fnamemodify(selection.path, ":p:h")
-                  require("telescope.actions").close(prompt_bufnr)
-                  -- Depending on what you want put `cd`, `lcd`, `tcd`
-                  vim.cmd(string.format("silent lcd %s", dir))
-                end
-              }
-            }
-          },
-          colorscheme = {
-            enable_preview = true
-          },
-        },
-
-      },
-    })
+    require('telescope').setup(telescope_opts)
 
     -- Mappings
     vim.keymap.set(
@@ -112,6 +114,7 @@ return {
       { desc = '[F]uzzily [/] search in current buffer' }
     )
 
+    vim.keymap.set('n', '<leader>ttv', function() _G.TelescopeToggleVendorIgnore()end, { desc = '[T]elescope [T]oggle [V]endor in search' })
     vim.keymap.set('n', '<leader>fa', require('telescope.builtin').live_grep, { desc = '[F]ind [A]all' })
     vim.keymap.set('v', "<leader>fa", require("telescope-live-grep-args.shortcuts").grep_visual_selection)
     --vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers, { desc = '[F]ind [B]uffers' })
@@ -151,5 +154,8 @@ return {
     vim.keymap.set('n', '<leader>fgb', require('telescope.builtin').git_branches, { desc = '[F]ind [G]it [B]ranches' })
     vim.keymap.set('n', '<leader>fgc', require('telescope.builtin').git_commits, { desc = '[F]ind [G]it [C]ommits' })
     vim.keymap.set('n', '<leader>fgs', require('telescope.builtin').git_stash, { desc = '[F]ind [G]it [S]tashes' })
+
+    -- Session via Persisted.nvim
+    vim.keymap.set('n', '<leader>so', ":Telescope persisted<cr>", { desc = '[S]ession [O]pen (via Persisted)' })
   end
 }

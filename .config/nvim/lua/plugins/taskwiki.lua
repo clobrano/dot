e991814e315628e6b14979dfe78dea3acedbdfcc
Carlo Lobrano <c.lobrano@gmail.com>
2025-07-05 15:56:00 +0200

    nvim: fixes for taskwiki

--- .config/nvim/lua/plugins/taskwiki.lua
+++ .config/nvim/lua/plugins/taskwiki.lua
@@ -5,20 +5,39 @@ return {
       let g:taskwiki_disable_concealcursor="yes"
     ]]
     vim.keymap.set('n', '<leader>rr', ':TaskWikiBufferLoad<cr>')
-    vim.api.nvim_create_autocmd("BufEnter", {
-      pattern = "*.md",  -- Match only markdown buffers
-      callback = function()
-        vim.cmd("TaskWikiBufferLoad")
-        vim.cmd("echo 'Tasks reloaded'")
-      end,
-    })
 
     local cwd = vim.fn.getcwd()
-    if cwd:find('^/home/clobrano')  then
-      print('work machine')
+
+    if cwd:find('^/home/clobrano') then
       vim.cmd [[
         let g:taskwiki_extra_warriors={ 'W': {'data_location': '/home/clobrano/Documents/taskwarriorRH/', 'taskrc_location': '/home/clobrano/.taskworkrc'}}
       ]]
+
+      -- Define the base path for your Taskwiki files
+      local work_journal_data_location = '/home/clobrano/Documents/RedHatNotes'
+      local personal_journal_data_location = '/home/clobrano/Me/Notes'
+
+      -- Create the autocommand ONLY if the buffer's path is within the journal_data_location
+      vim.api.nvim_create_autocmd("BufEnter", {
+        -- Pattern to match markdown files
+        pattern = "*.md",
+        -- Use a Lua condition to check the file path
+        group = vim.api.nvim_create_augroup("TaskwikiBufferLoad", { clear = true }), -- Good practice to use an augroup
+        callback = function(args)
+          local file_path = vim.api.nvim_buf_get_name(args.buf)
+          -- Check if the file_path starts with the journal_data_location
+          if file_path:sub(1, #work_journal_data_location) == work_journal_data_location then
+            vim.cmd("TaskWikiBufferLoad")
+          elseif file_path:sub(1, #personal_journal_data_location) == personal_journal_data_location then
+            vim.cmd("TaskWikiBufferLoad")
+          else
+            print(file_path .. " is not a configured journal location")
+          end
+        end,
+      })
+    else
+      -- If not on the work machine, do nothing or handle other configs
+      print('not a work machine, skipping taskwiki_extra_warriors config')
     end
   end
 }

74a56217489396a2651f8549cea8b41479694fff
Carlo Lobrano <c.lobrano@gmail.com>
2025-07-09 16:16:30 +0200

    nvim:codecompanion: refreshed configuration file

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -4,15 +4,38 @@ return {
     { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
     { "nvim-lua/plenary.nvim" }
   },
-  opts = {
-    --Refer to: https://github.com/olimorris/codecompanion.nvim/blob/main/lua/codecompanion/config.lua
-    strategies = {
-      --NOTE: Change the adapter as required
-      chat = { adapter = "gemini" },
-      inline = { adapter = "gemini" },
-    },
-    opts = {
-      log_level = "DEBUG",
-    },
-  },
+  -- https://github.com/deepseek-ai/awesome-deepseek-integration/blob/main/docs/codecompanion.nvim/README.md
+  config = function()
+    require('codecompanion').setup({
+      strategies = {
+        chat = { adapter = "gemini" },
+        inline = { adapter = "gemini" },
+      },
+      opts = {
+        log_level = "DEBUG",
+      },
+      prompt_library = {
+        ["Code Review TBD"] = {
+          strategy = "chat",
+          description = "review local changes",
+          prompts = {
+            {
+              role = "system",
+              content = "You are an experienced developer with Lua and Neovim",
+            },
+            {
+              role = "user",
+              content = "Can you explain why ..."
+            }
+          },
+        },
+      },
+
+      vim.keymap.set("n", "<leader>Ca", ":CodeCompanionAction<cr>",
+        { desc = "[C]odecompanion [A]ction", silent = true, noremap = true }),
+    vim.keymap.set("n", "<leader>Cc", ":CodeCompanionChat<cr>",
+        { desc = "[C]odecompanion [C]hat", silent = true, noremap = true })
+
+    })
+  end
 }

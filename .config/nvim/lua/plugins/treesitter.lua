68c50c51d858174b06c4e64452cca6c5ec421e2b
Carlo Lobrano <c.lobrano@gmail.com>
2026-01-07 15:12:55 +0100

    nvim:treesitter: call vim.treesitter.start() in markdown files

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -8,7 +8,7 @@ return {
     vim.api.nvim_create_autocmd("FileType", {
       group = treesitter_group,
       -- Add any filetypes you want to support in this list
-      pattern = { "go", "lua", "python", "rust", "javascript", "typescript", "cpp", "c" },
+      pattern = {  "markdown", "go", "lua", "python", "rust", "javascript", "typescript", "cpp", "c" },
       callback = function()
         vim.treesitter.start()
       end,

588d3590fea81d52ad2c2b424ee3c879a1e2e72c
Carlo Lobrano <c.lobrano@gmail.com>
2026-06-01 15:05:32 +0200

    nvim:treesitter: enabled the plugin as the best to install languages

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,4 +1,4 @@
 return {
   'nvim-treesitter/nvim-treesitter',
-  enabled = false, -- Treesitter is now built-in to Neovim 0.12, plugin not needed
+  enabled = true, -- Treesitter is now built-in to Neovim 0.12, plugin not needed
 }

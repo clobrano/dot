a716b37ff57629b6a66f62b100286ce528985509
Carlo Lobrano <c.lobrano@gmail.com>
2026-06-19 09:08:20 +0200

    nvim: add image and luarocks to support in-terminal pictures

---
+++ .config/nvim/lua/plugins/image.lua
@@ -0,0 +1,16 @@
+return {
+  "3rd/image.nvim",
+  dependencies = { "luarocks.nvim" },
+  config = function()
+    require("image").setup {
+      backend = "kitty",
+      integrations = {
+        markdown = {
+          enabled = true,
+          sizing_strategy =
+          "fit_image_while_staying_above_window_baseline"
+        }
+      }
+    }
+  end
+}

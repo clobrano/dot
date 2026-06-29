d986b8cecc6c0836e642174dfb81982913a650c9
Carlo Lobrano <c.lobrano@gmail.com>
2026-06-29 15:41:12 +0200

    golang: update install app list

--- .config/cconf/environments/golang/apps.sh
+++ .config/cconf/environments/golang/apps.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # -*- coding: UTF-8 -*-
-go install github.com/clobrano/readitlater@latest
 go install github.com/clobrano/BookmarkIt@latest
 go install github.com/clobrano/wui@latest
 go install github.com/clobrano/LogBook@latest
+go install github.com/clobrano/prow-helper@latest
+go install github.com/clobrano/fuzzy-cd@latest
+#go install github.com/clobrano/readitlater@latest

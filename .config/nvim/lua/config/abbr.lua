3f323f6549d753b9551ba41a46617a8a2010439a
Carlo Lobrano <c.lobrano@gmail.com>
2026-07-24 08:42:04 +0200

    nvim: `ddy` shortcut made more general
    From (WEEKDAY) (e.g. `(FRI)`), to Weekday (e.g. Fri)

--- .config/nvim/lua/config/abbr.lua
+++ .config/nvim/lua/config/abbr.lua
@@ -2,5 +2,5 @@ vim.cmd([[
 iabbrev <expr> ddate strftime("%F")
 iabbrev <expr> hdate strftime("%A %d %B")
 iabbrev <expr> dday strftime("%A")
-iabbrev <expr> ddy toupper(strftime("(%a)"))
+iabbrev <expr> ddy strftime("%a")
 ]])
